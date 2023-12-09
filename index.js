ee5777772010b644a3efa5c2dde3feb5d93bf2d6
Анастасия Зимина <Asya.Zimina777@mail.ru>
2023-12-09 16:02:01 +0300

    refactor api

--- index.js
+++ index.js
@@ -1,57 +1,25 @@
-class Progress {
-  constructor(parentNode, value, isAnimated, isHidden) {
-    this._parentNode = parentNode;
-    this._value = value;
-    this._isAnimated = isAnimated;
-    this._isHidden = isHidden;
-  }
+const progress = new Progress('#parent', 20, true, false);
+progress.render();
 
-  getTemplate() {
-    this._element = document.querySelector('.progress-template').content.querySelector('.progress-ring').cloneNode(true);
-  }
+const inputPercent = document.querySelector('#progress__percent');
+inputPercent.value = progress._value;
+const inputAnimate = document.querySelector('#progress__animate');
+inputAnimate.checked = progress._isAnimated;
+const inputHide = document.querySelector('#progress__hide');
+inputHide.checked = progress._isHidden;
 
-  generateProgress() {
-    let progressRingCircle = this._element.querySelector('.progress-ring__circle');
-    if (this._isAnimated) {
-      progressRingCircle.style.animation = '2s linear 0s normal none infinite running rotation';
-    } else {
-      progressRingCircle.style.animation = '';
-    }
-    if (this._isHidden) {
-      this._element.style.visibility = 'hidden';
-    } else {
-      this._element.style.visibility = 'visible';
-    }
-    const radius = progressRingCircle.r.baseVal.value;
-    const circumference = 2 * Math.PI * radius;
-    progressRingCircle.style.strokeDasharray = `${circumference} ${circumference}`;
-    progressRingCircle.style.strokeDashoffset = `${circumference}`;
-    const offset = circumference - this._value / 100 * circumference;
-    progressRingCircle.style.strokeDashoffset = `${offset}`;
-    return this._element;
-  }
+inputPercent.addEventListener('change', function () {
+  progress.setValue(inputPercent.value);
+})
 
-  setValue(value) {
-    this._value = value;
-    this.generateProgress();
-  }
+inputAnimate.addEventListener('change', function (evt) {
+  progress.setAnimated(evt.target.checked);
+})
 
-  onToggleAnimated() {
-    this._isAnimated = !this._isAnimated;
-    this.generateProgress();
-  }
+inputHide.addEventListener('change', function (evt) {
+  progress.setHide(evt.target.checked);
+})
 
-  onToggleHide() {
-    this._isHidden = !this._isHidden;
-    this.generateProgress();
-  }
+const form = document.querySelector('.progress__form');
+form.addEventListener('submit', (evt)=>evt.preventDefault())
 
-  render() {
-    this.getTemplate()
-    let progressElement = this.generateProgress();
-    document.querySelector(`${this._parentNode}`).append(progressElement);
-  }
-}
-
-const secondProgress = new Progress('#root', 20, true, false);
-secondProgress.render();
---
+++ progressAPI.js
@@ -0,0 +1,80 @@
+class Progress {
+  constructor(parentNode, value, isAnimated, isHidden) {
+    this._parentNode = parentNode;
+    this._value = value;
+    this._isAnimated = isAnimated;
+    this._isHidden = isHidden;
+  }
+
+  getTemplate() {
+    this._element = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
+    this._element.setAttribute('class', 'progress-ring');
+    const progressRing = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
+    progressRing.setAttribute('class', 'progress-ring__circle');
+    progressRing.setAttribute('cx', '55');
+    progressRing.setAttribute('cy', '55');
+    progressRing.setAttribute('r', '49');
+    const progressRingShadow = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
+    progressRingShadow.setAttribute('class', 'progress-ring__circle_shadow');
+    progressRingShadow.setAttribute('cx', '55');
+    progressRingShadow.setAttribute('cy', '55');
+    progressRingShadow.setAttribute('r', '49');
+    this._element.append(progressRingShadow);
+    this._element.append(progressRing);
+    return this._element;
+  }
+
+  generateValue() {
+    let progressRingCircle = this._element.querySelector('.progress-ring__circle');
+    const radius = progressRingCircle.r.baseVal.value;
+    const circumference = 2 * Math.PI * radius;
+    progressRingCircle.style.strokeDasharray = `${circumference} ${circumference}`;
+    progressRingCircle.style.strokeDashoffset = `${circumference}`;
+    const offset = circumference - this._value / 100 * circumference;
+    progressRingCircle.style.strokeDashoffset = `${offset}`;
+  }
+
+  generateAnimate() {
+    let progressRingCircle = this._element.querySelector('.progress-ring__circle');
+    if (this._isAnimated) {
+      progressRingCircle.style.animation = '2s linear 0s normal none infinite running rotation';
+    } else {
+      progressRingCircle.style.animation = '';
+    }
+  }
+
+  generateHidden() {
+    if (this._isHidden) {
+      this._element.style.visibility = 'hidden';
+    } else {
+      this._element.style.visibility = 'visible';
+    }
+  }
+
+  setValue(value) {
+    this._value = value;
+    this.generateValue();
+  }
+
+  setAnimated(isAnimated) {
+    this._isAnimated = isAnimated;
+    this.generateAnimate();
+  }
+
+  setHide(isHidden) {
+    this._isHidden = isHidden;
+    this.generateHidden();
+  }
+
+  render() {
+    let progressElement = this.getTemplate();
+    this.generateValue();
+    this.generateValue();
+    this.generateAnimate();
+    document.querySelector(`${this._parentNode}`).append(this._element);
+  }
+
+  unmount() {
+    this._element.remove();
+  }
+}
